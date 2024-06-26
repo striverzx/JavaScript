b5d3466f7ec649e92a5edc85fcc5b69e87a711c7
striverzx <striverzx011@gmail.com>
2024-06-26 20:05:46 +0530

    the file updated

---
+++ 06HigherOrderFunctions/Map.js
@@ -0,0 +1,8 @@
+const arr=[1,2,3,4,5]
+
+
+function double(x){
+    return x*2;
+}
+const output1=arr.map(double);
+console.log(output1);
--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-{
-    var a=10;
-    
+setTimeout(function(){
+    console.log("timer after 5 sec")
 
+},5000);
+
+
+
+function y(a){
+    console.log("y");
+    a();
 }
 
-{
-    var a=1;
-    
-    
+y(function(){
+    console.log("x");
+})
 
-}
-
-{
-    console.log(a);
-
-}
